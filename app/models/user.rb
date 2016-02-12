2aff27756caf9eeaa9096a2180830e734ce18adc
carlosprovisorio <carlosprovisorio@icloud.com>
2016-02-12 09:18:30 -0500

    Added tests for user first name, last name, and profile correctness.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,17 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_many :statuses
+
+validates :first_name, presence: true
+validates :last_name, presence: true
+validates :profile_name, presence: true,
+                          uniqueness: true,
+                          format: {
+                          with: /a-zA-Za0-9_-/,
+                          message: 'Must be formatted correctly'
+                        }
+
+has_many :statuses
 
 
 
