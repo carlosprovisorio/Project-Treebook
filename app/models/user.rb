d11fc1cfce6891eaaa9891ace81e35e3b2c3a1a2
carlosprovisorio <carlosprovisorio@icloud.com>
2016-02-13 15:42:26 -0500

    Fixed bug with user profile validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,8 +10,8 @@ validates :last_name, presence: true
 validates :profile_name, presence: true,
                           uniqueness: true,
                           format: {
-                          with: /a-zA-Za0-9_-/,
-                          message: 'Must be formatted correctly'
+                          with: /\A[a-zA-Z0-9_\-]+\z/,
+                          message: 'Must be formatted correctly.'
                         }
 
 has_many :statuses
